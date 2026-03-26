8b438d74bcd594ddaed465f93c99b6dd75b23401
John <23259@wc.school.nx>
2026-03-27 11:07:50 +1300

    Added a proper enemy

---
+++ Scenes/enemy_1.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	#randomly jump
+	
+	if randf()>0.99 and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if $"../Charectir".global_position.distance_to(global_position) < 500 and $"../Charectir".global_position.distance_to(global_position) > 250:
+		
+		
+		if $"../Charectir".position > global_position:
+			velocity.x = SPEED
+			$AnimatedSprite2D.flip_h = false
+		
+		else:
+			velocity.x = -SPEED
+			$AnimatedSprite2D.flip_h = true
+
+	else:
+		
+		velocity.x = 0
+	
+	if $"../Charectir".global_position.distance_to(global_position) > 250:
+		print("Shoot")
+		
+		$AnimatedSprite2D.play("Shoot")
+	
+	
+	
+	if velocity.x == 0:
+		$AnimatedSprite2D.play("Shoot")
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
