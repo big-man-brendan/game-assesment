78b6202c43fe3b2492803bfb14299b48e7143e43
John <23259@wc.school.nx>
2026-03-31 09:39:08 +1300

    PEP 8. not

---
+++ Scripts/bullet_1.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+var hits = 0
+var shooting_dir = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	position.x -= 10 * shooting_dir 
+	
+
+func _on_area_entered(area: Area2D) -> void:
+	
+	
+	hits += 1
+	
+	if hits == 2:
+		print("Hit")
+		hits = 0
+		position.x = 100000
+		
+		
+
+
+func _on_enemy_1_shoot() -> void:
+	
+	hits = 0
+	
+	print("bullet shoot")
+	
+	position = $"../Enemy1".position
+	
+	if $"../Enemy1".is_flipped == false:
+		shooting_dir = -1
+		
+	else:
+		shooting_dir = 1
+		
+	
+	
--- Scripts/enemy_1.gd
+++ Scripts/enemy_1.gd
@@ -3,13 +3,28 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
+var is_flipped = false
+var shot = false
 
+signal shoot()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	
+	if $"../Charectir".position > global_position:
+		velocity.x = SPEED
+		$AnimatedSprite2D.flip_h = false
+		is_flipped = false
+	
+	else:
+			
+		$AnimatedSprite2D.flip_h = true
+		is_flipped = true
+	
+	
 	#randomly jump
 	
 	if randf()>0.99 and is_on_floor():
@@ -21,21 +36,26 @@ func _physics_process(delta: float) -> void:
 		if $"../Charectir".position > global_position:
 			velocity.x = SPEED
 			$AnimatedSprite2D.flip_h = false
+			is_flipped = false
 		
 		else:
 			velocity.x = -SPEED
 			$AnimatedSprite2D.flip_h = true
+			is_flipped = true
 
 	else:
 		
 		velocity.x = 0
 	
-	if $"../Charectir".global_position.distance_to(global_position) > 250:
-		print("Shoot")
+	if $"../Charectir".global_position.distance_to(global_position) < 280:
+		
 		
 		$AnimatedSprite2D.play("Shoot")
-	
-	
+		if shot == false:
+			print("Shoot")
+			emit_signal("shoot")
+			shot = true
+			$Shottimer.start()
 	
 	if velocity.x == 0:
 		$AnimatedSprite2D.play("Shoot")
@@ -49,3 +69,7 @@ func _physics_process(delta: float) -> void:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_shottimer_timeout() -> void:
+	shot = false
